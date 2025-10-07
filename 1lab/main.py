7297fc7ae95c488f3ba617fb8bc9cfa488109e7a
icebergfire <vladtshay1995@gmail.com>
2025-10-07 18:23:57 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -6,8 +6,8 @@ counts = [[0]*5 for _ in range(5)]
 for i in range(1000):
     numbers = [1, 2, 3, 4, 5]
     random.shuffle(numbers)  
-for j in range(5):
-    num = numbers[j]
-    counts[num - 1][j] += 1
+    for j in range(5):
+        num = numbers[j]
+        counts[num - 1][j] += 1
 for i in range(5):
     print(counts[i])
