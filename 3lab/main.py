a2096a3b1f18baf0f190f8f56f0ed7fc469be5f5
icebergfire <vladtshay1995@gmail.com>
2025-11-17 10:49:21 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,14 @@
+steps = []
 
+def permute(current, remaining):
+    steps.append((current.copy(), remaining.copy()))
+    if not remaining:
+        print("Готовая перестановка:", current)
+        return
+    for i in range(len(remaining)):
+        permute(current + [remaining[i]], remaining[:i] + remaining[i+1:])
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+permute([], [1, 2, 3])
+print("История шагов:")
+for step in steps:
+    print(step)
